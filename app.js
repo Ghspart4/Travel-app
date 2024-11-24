e3904440fb5490bd86030662e79adc8a68fda473
jordan martinez <martinezlollito@gmail.com>
2024-11-24 17:39:34 +1300

    public update

---
+++ app.js
@@ -0,0 +1,16 @@
+// Importing the required modules
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.set('view engine', 'ejs');
+// Correct route handler:
+app.get('/', (req, res) => {
+    res.render('index'); // Correctly renders the 'index.ejs' template
+  });
+  
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${3000}/`);
+});
